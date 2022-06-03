15f37542d9d782f5b78ee4bf76e651d7ef658c96
nenads97 <nenad.suknovicpr143@gmail.com>
2022-06-03 12:14:23 +0200

    PrviCommit

---
+++ FitensCentar/Controllers/KorisnikController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FitensCentar.Models;
+
+namespace FitensCentar.Controllers
+{
+    public class KorisnikController : Controller
+    {
+        // GET: Korisnik
+        public ActionResult Korisnik()
+        {
+            var korisnik = new Korisnik()
+            {
+                username = "Nenad", datumRodjenja = DateTime.Now, password = "aezakmi",
+                emailKorisnika = "nenad.suknovic@gmail.com", imeKorisnika = "Nenad", pol = 0,
+                prezimeKorisnika = "Suknovic", uloga = 0
+
+            };
+                return View();
+        }
+    }
+}
---
+++ FitensCentar/Models/Korisnik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitensCentar.Models
+{
+    public class Korisnik
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string imeKorisnika { get; set; }
+        public string prezimeKorisnika { get; set; }
+        public Pol pol { get; set; }
+        public string emailKorisnika { get; set; }
+        public DateTime datumRodjenja  { get; set; }
+        public Uloga uloga { get; set; }
+    }
+}

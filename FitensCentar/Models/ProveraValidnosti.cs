e553e6fa75d4d7e15248f2318107f77a42ab31b1
nenads97 <nenad.suknovicpr143@gmail.com>
2022-08-15 13:48:58 +0200

    Provera Validnosti, Dorada Singlton pristupa podacima, XMLFileController

---
+++ FitensCentar/Models/ProveraValidnosti.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitensCentar.Models
+{
+    public class Validator
+    {
+        private static Validator validator;
+
+        private Validator()
+        { }
+
+        public static Validator Instanca()
+        {
+            if (validator == null)
+            {
+                validator = new Validator();
+            }
+            return validator;
+        }
+
+        public bool PrazanStringValidator (string str, out string poruka)
+        {
+            poruka = "";
+            if (str == "")
+            {
+                poruka = "*Ovo polje ne sme biti prazno.";
+                return true;
+            }
+            return false;
+        }
+
+        public bool SifraStringValidator(string str, out string poruka)
+        {
+            poruka = "";
+            if (str.Length > 0 && str.Length < 8)
+            {
+                poruka = "*Lozinka mora sadržati minimalno 8 karaktera.";
+                return false;
+            }
+
+            char[] niz = str.ToCharArray();
+            for (int i = 0; i < niz.Length; i++)
+            {
+                if (!JesteRazmak(niz[i]) || !JesteSlovo(niz[i]))
+            }
+        }
+
+        public bool JesteRazmak(char c)
+        {
+            if (c == 32)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool JesteSlovo(char c)
+        {
+            if ((c >= 65 && c <= 90) || ( c >= 97 && c <= 122))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool JesteBroj(char c)
+        {
+            if (c >= 48 && c <= 57)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private string PronadjiTrenera (string username, string password)
+        {
+            if ((SingltonPristupPodacima.Instance()))
+        }
+    }
+}
--- FitensCentar/Models/SingltonPristupPodacima.cs
+++ FitensCentar/Models/SingltonPristupPodacima.cs
@@ -10,5 +10,115 @@ namespace FitensCentar.Models
         private static SingltonPristupPodacima instanca;
 
         public List<Vlasnik> _Vlasnici { get; protected set; }
+        public List<Posetilac> _Posetioci { get; protected set; }
+        public List<Trener> _Treneri { get; protected set; }
+
+        private SingletonPristupPodacima()
+        {
+            UcitajPodatke();
+            DodajVlasnikaNaPocetku();
+            InicijalizujKolekcije();
+        }
+
+        private void UcitajPodatke()
+        {
+            CitajVlasnike();
+            CitajPosetioce();
+            CitajTrenere();
+        }
+
+        #region Pisi/Citaj
+        private void PisiVlasnike()
+        {
+            XMLFileController.SerializujVlasnike(new Vlasnici(_Vlasnici));
+        }
+
+        private void PisiPosetioce()
+        {
+            XMLFileController.SerializujPosetioce(new Posetioci(_Posetioci));
+        }
+
+        private void PisiTrenere()
+        {
+            XMLFileController.SerializujTrenere(new Treneri(_Treneri));
+        }
+
+        private List<Vlasnik> CitajVlasnike()
+        {
+            Vlasnici vlasnici = null;
+            if ((vlasnici = XMLFileController.DeserijalizujVlasnike()) != null)
+            {
+                _Vlasnici = vlasnici.DobaviVlasnike();
+            }
+            return _Vlasnici;
+        }
+
+        private List<Posetilac> CitajPosetioce()
+        {
+            Posetioci posetioci = null;
+            if ((posetioci = XMLFileController.DeserijalizujPosetioce()) != null)
+            {
+                _Posetioci = posetioci.DobaviPosetioce();
+            }
+            return _Posetioci;
+        }
+
+        private List<Trener> CitajTrenere()
+        {
+            Treneri treneri = null;
+            if ((treneri = XMLFileController.DeserijalizujTrenere()) != null)
+            {
+                _Treneri = treneri.DobaviTrenere();
+            }
+            return _Treneri;
+        }
+        #endregion
+
+        #region Dodaj
+
+        #region DodajJednog
+        public void DodajPosetioca(Posetilac p)
+        {
+            _Posetioci.Add(p);
+            object posetilac = p;
+            PisiPosetioce();
+        }
+
+        public void DodajTrenera(Trener t)
+        {
+            _Treneri.Add(t);
+            object trener = t;
+            PisiTrenere();
+        }
+
+        public void DodajVlasnika(Vlasnik v)
+        {
+            _Vlasnici.Add(v);
+            object vlasnik = v;
+            PisiVlasnike();
+        }
+
+        public void DodajFitnesCentar(FitnesCentar f)
+        {
+            Trener t = _Treneri.Find(x => x.Username == f.Vlasnik.Username);
+            if (t != null)
+            {
+                t.ftCentri.Add(f);
+                object fitnesCentar = f;
+                PisiTrenere();
+            }
+        }
+
+        /*public void DodajGrupniTrening(GrupniTrening gt)
+        {
+           
+        }
+        */
+        #endregion
+
+        #region DodajVise
+        #endregion
+
+        #endregion
     }
 }
